a96f0f9d9fd50b2160c9aaf50cff3ef8c38ece49
GYMing <gyming@outlook.com>
2017-01-19 04:42:03 +0800

    简单配置了一下服务器，以后会进行优化

---
+++ app.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var path = require('path');
+var http = require('http');
+
+var users = require('./routes/users/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, './src/views'));
+app.set('view engine', 'pug');
+
+// 设置静态资源的目录，比如 css js 等文件的路径
+app.use(express.static(path.join(__dirname, './src')));
+
+// 覆盖默认的 X-Powered-By HTTP返回头
+app.use(function (req, res, next) {
+    res.set('X-Powered-By','USA-Client-Manage-Platform');
+    next();
+});
+
+app.use('/', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
