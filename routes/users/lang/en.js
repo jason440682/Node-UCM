0b8170f9ba80b7aa466b44af25a83233baf3021e
GYMing <gyming@outlook.com>
2017-01-19 04:43:58 +0800

    针对中文和英文两种不同语言对页面进行渲染

---
+++ routes/users/lang/en.js
@@ -0,0 +1,137 @@
+/**
+ * Created by shoufu on 17/1/19.
+ */
+
+module.exports = {
+    title: 'Create Stuff User',
+
+    username: {
+        text: 'Username',
+        placeholder: 'Text123'
+    },
+
+    password: {
+        text: 'Password',
+        placeholder: 'password here'
+    },
+
+    confirm: {
+        text: 'Confirm Password',
+        placeholder: 'confirm please!'
+    },
+
+    question: {
+        text: 'Security Question',
+        placeholder: 'set your question'
+    },
+
+    answer: {
+        text: 'Security Question Answer',
+        placeholder: 'The answer of the question'
+    },
+
+    auto_login: {
+        text: 'Enable Two-Factor Authentication Login',
+        yes: 'Yes',
+        no: 'No'
+    },
+
+    send_code: {
+        text: 'Send passcode to',
+        mobile: 'Mobile Phone',
+        email: 'Email'
+    },
+
+    first_name: {
+        text: 'First Name',
+        placeholder: 'First Name'
+    },
+
+    last_name: {
+        text: 'Last Name',
+        placeholder: 'Last Name'
+    },
+
+    email_address: {
+        text: 'Email Address',
+        placeholder: 'Your Email Address'
+    },
+
+    mobile_num: {
+        text: 'Mobile Phone Number',
+        placeholder: 'xxx-xxx-xxxx'
+    },
+
+    other_num: {
+        text: 'Other Phone Number',
+        placeholder: 'xxx-xxx-xxxx'
+    },
+
+    job_title: {
+        text: 'Job Title',
+        placeholder: 'Text'
+    },
+
+    department: {
+        text: 'Department/Division',
+        departments: [
+            'Billing Department 1',
+            'Billing Department 2',
+            'Billing Department 3',
+            'Billing Department 4',
+            'Billing Department 5'
+        ]
+    },
+
+    time_zone: {
+        text: 'Work Time Zone',
+        timeZones: [
+            'US Pacific Time 1',
+            'US Pacific Time 2',
+            'US Pacific Time 3',
+            'US Pacific Time 4',
+            'US Pacific Time 5'
+        ]
+    },
+
+    work_email: {
+        text: 'Work Email',
+        placeholder: 'Text'
+    },
+
+    office_phone: {
+        text: 'Office Phone Number',
+        placeholder: 'Text'
+    },
+
+    office_address: {
+        text: 'Office Address',
+        placeholder: {
+            street: 'Street',
+            city: 'City',
+            state: 'State',
+            zip_code: 'Zip Code'
+        },
+        country: [
+            'China 1',
+            'China 2',
+            'China 3',
+            'China 4',
+            'China 5'
+        ]
+    },
+
+    user_note: {
+        text: 'User Note',
+        placeholder: 'Text'
+    },
+
+    photo: {
+        text: 'Personal Photo',
+        placeholder: 'Upload'
+    },
+
+    submit_btn: {
+        text: 'Add Staff'
+    }
+};
---
+++ routes/users/lang/zh.js
@@ -0,0 +1,119 @@
+/**
+ * Created by shoufu on 17/1/19.
+ */
+
+module.exports = {
+    title: ' 创建新用户',
+
+    username: {
+        text: '用户名',
+        placeholder: '请输入用户名'
+    },
+
+    password: {
+        text: '密码',
+        placeholder: '请输入密码'
+    },
+
+    confirm: {
+        text: '重复输入密码',
+        placeholder: '请再次输入密码'
+    },
+
+    question: {
+        text: '设置密保问题',
+        placeholder: '请设置您的问题'
+    },
+
+    answer: {
+        text: '设置密保答案',
+        placeholder: '请设置您的答案'
+    },
+
+    auto_login: {
+        text: '',
+        yes: '',
+        no: ''
+    },
+
+    send_code: {
+        text: '',
+        mobile: '',
+        email: ''
+    },
+
+    first_name: {
+        text: '',
+        placeholder: ''
+    },
+
+    last_name: {
+        text: '',
+        placeholder: ''
+    },
+
+    email_address: {
+        text: '',
+        placeholder: ''
+    },
+
+    mobile_num: {
+        text: '',
+        placeholder: ''
+    },
+
+    other_num: {
+        text: '',
+        placeholder: ''
+    },
+
+    job_title: {
+        text: '',
+        placeholder: ''
+    },
+
+    department: {
+        text: '',
+        departments: []
+    },
+
+    time_zone: {
+        text: '',
+        timeZones: []
+    },
+
+    work_email: {
+        text: '',
+        placeholder: ''
+    },
+
+    office_phone: {
+        text: '',
+        placeholder: ''
+    },
+
+    office_address: {
+        text: '',
+        placeholder: {
+            street: '',
+            city: '',
+            state: '',
+            zip_code: ''
+        },
+        country: []
+    },
+
+    user_note: {
+        text: '',
+        placeholder: ''
+    },
+
+    photo: {
+        text: '',
+        placeholder: ''
+    },
+
+    submit_btn: {
+        text: ''
+    }
+};
