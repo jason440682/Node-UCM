487bb94a44fb949c012c618bd480d746333f7e56
GYMing <gyming@outlook.com>
2017-01-22 01:58:17 +0800

    在首页对语言进行了默认的选择，代码写的不是很好，等所有页面重构好了以后在进行优化

---
+++ routes/index.js
@@ -0,0 +1,45 @@
+/**
+ * 这个页面主要负责主页的展示内容
+ */
+var express = require('express');
+var router = express.Router();
+
+var users = require('./users');
+var client = require('./client');
+
+router.get('/', function (req, res, next) {
+    req.lang = 'en';
+    res.redirect('/en/login');
+});
+
+router.get('/en', function (req, res, next) {
+    req.lang = 'en';
+    res.redirect('/en/login');
+});
+
+router.get('/cn', function (req, res, next) {
+    req.lang = 'zh-cn';
+    res.redirect('/cn/login');
+});
+
+router.use('/en/', function (req, res, next) {
+    req.lang = 'en';
+    next();
+}, users);
+
+router.use('/cn/', function (req, res, next) {
+    req.lang = 'zh-cn';
+    next();
+}, users);
+
+router.use('/en/client', function (req, res, next) {
+    req.lang = 'en';
+    next();
+}, client);
+
+router.use('/cn/client', function (req, res, next) {
+    req.lang = 'zh-cn';
+    next();
+}, client);
+
+module.exports = router;
