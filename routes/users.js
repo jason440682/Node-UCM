9d4d6500be377a8f9b62149951cd427f3ad208fb
GYMing <gyming@outlook.com>
2017-01-22 02:00:20 +0800

    对用户的 router 匹配进行了修改

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,13 @@
 /**
  *  这个路由页面主要负责跟用户相关的界面，比如用户注册、登录等
  */
-
 var express = require('express');
 var router = express.Router();
 
-var register = require('./users/register');
-var signup = require('./users/signup');
+var register = require('./users/signup');
+var login = require('./users/login');
 
-router.use('/', register);
-router.use('/signup', signup);
+router.use('/login', login);
+router.use('/register', register);
 
 module.exports = router;
