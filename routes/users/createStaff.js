7f495f1734b07612e0a7b910b1009531ca5e19a8
GYMing <gyming@outlook.com>
2017-01-22 01:03:11 +0800

    原来的 register 的名字有错，现更改为 createStaff

---
+++ routes/users/createStaff.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+    // 这个 data 是要渲染到 Jade 模板的基本的数据
+    var data = {
+        // key 对应的是模板
+        key: 'create',
+        title: 'Create Staff User'
+    };
+
+    if (req.lang && req.lang == 'zh') {
+        data.language = 'zh-cn';
+        data.lang = require('./lang/zh-cn/createStaff');
+    } else {
+        data.language = 'en';
+        data.lang = require('./lang/en/createStaff');
+    }
+
+    res.render('client/create', data);
+});
+
+router.post('/', function (req, res, next) {
+    // 这里写数据页面 POST 数据过来时要处理的逻辑
+});
+
+module.exports = router;
