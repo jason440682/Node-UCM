dabb71b9ef68cc2602449e5577438922bb45a896
GYMing <gyming@outlook.com>
2017-02-21 18:32:37 +0800

    完成 docManager 页面的开发

--- routes/client/accounts/docManager.js
+++ routes/client/accounts/docManager.js
@@ -1,27 +1,61 @@
 var express = require('express');
 var router = express.Router();
 
+var data_eg = {
+    names: ['James Johnson', 'Payment Reminder', 'Payment Reminder2'],
+    files: [
+        {type: 'folder', name: 'folder1', list: [
+            {type: 'folder', name: 'file1-1', list: [
+                {type: 'file', name: 'file1-1-1'},
+                {type: 'file', name: 'file1-1-2'},
+                {type: 'file', name: 'file1-1-3'},
+                {type: 'file', name: 'file1-1-4'}
+            ]},
+            {type: 'file', name: 'file1-2'},
+            {type: 'file', name: 'file1-3'}
+        ]},
+        {type: 'folder', name: 'folder2', list: [
+            {type: 'file', name: 'file2-1'},
+            {type: 'file', name: 'file2-2'},
+            {type: 'file', name: 'file2-3'}
+        ]},
+        {type: 'folder', name: 'folder3', list: [
+            {type: 'file', name: 'file3-1'},
+            {type: 'file', name: 'file3-2'},
+            {type: 'file', name: 'file3-3'}
+        ]},
+        {type: 'file', name: 'file2'},
+        {type: 'file', name: 'file3'},
+        {type: 'file', name: 'file4'}
+    ]
+};
+
 router.get('/', function (req, res, next) {
-  var data = {
-    key: 'accounts/docManager',
-    title: 'Clent Account',
-    nav: require('../public/lang/en/navbar')
-  };
+    var data = {
+        key: 'accounts/docManager',
 
-  if (req.lang && req.lang == 'zh-cn') {
-    data.language = 'zh-cn';
-    data.lang = require('././zh-cn/accounts');
-  } else {
-    data.language = 'en';
-    data.lang = require('./lang/en/notification');
-  }
+        language: 'en',
+        lang: require('./lang/en/docManager'),
+        nav: require('../public/lang/en/navbar'),
+        footnavbar: require('../public/lang/en/footbavbar'),
 
-  res.render('client/accounts/docManager', data);
+        names: data_eg.names,
+        files: data_eg.files
+    };
+
+    if (req.lang && req.lang == 'zh-cn') {
+        data.language = 'zh-cn';
+        data.lang = require('./lang/zh-cn/docManager');
+        data.nav = require('../public/lang/zh-cn/navbar');
+        data.footnavbar = require('../public/lang/zh-cn/footnavbar');
+    }
+
+    res.render('client/accounts/docManager', data);
 });
 
 router.post('/', function (req, res, next) {
-  // 这里写注册页面 POST 数据过来时要处理的逻辑
-  console.log('post!');
+    // 这里写注册页面 POST 数据过来时要处理的逻辑
+    console.log('post!');
 });
 
 module.exports = router;
