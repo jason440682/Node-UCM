0cb8d25521d934b37142164a234bf72ea740c637
GYMing <gyming@outlook.com>
2017-02-21 18:33:32 +0800

    对已完成的界面进行了调整

--- routes/client.js
+++ routes/client.js
@@ -5,42 +5,44 @@ var express = require('express');
 var router = express.Router();
 
 var accounts = require('./client/accounts/clientAccounts');
-var createaccount = require('./client/accounts/createAccount');
+var createAccount = require('./client/accounts/createAccount');
 var notification = require('./client/accounts/notification');
 var documentManager = require('./client/accounts/docManager');
+var modifyAccount = require('./client/accounts/modifyAccount');
 var workbook = require('./client/workbook/workbook');
-var creatework = require('./client/workbook/createWork');
-var workcalendar = require('./client/workbook/workCalendar');
-var worktemplate = require('./client/workbook/workTemplate');
-var modifywork = require('./client/others/modifyWork');
-var billingpayment = require('./client/billingPayment/billingPayment');
-var createinvoice = require('./client/billingPayment/createInvoice');
+var createWork = require('./client/workbook/createWork');
+var workCalendar = require('./client/workbook/workCalendar');
+var workTemplate = require('./client/workbook/workTemplate');
+var modifyWork = require('./client/others/modifyWork');
+var billingPayment = require('./client/billingPayment/billingPayment');
+var createInvoice = require('./client/billingPayment/createInvoice');
 var invoices = require('./client/billingPayment/invoices');
 var payment = require('./client/billingPayment/payment');
-var modifyinvoice = require('./client/others/modifyInvoice');
+var modifyInvoice = require('./client/others/modifyInvoice');
 var newsletter = require('./client/marketing/newsletter');
-var createnewsletter = require('./client/marketing/createNewsletter');
+var createNewsletter = require('./client/marketing/createNewsletter');
 var createStaff = require('./client/user/createStaff');
 
 router.use('/accounts', accounts);
-router.use('/createaccount', createaccount);
+router.use('/createAccount', createAccount);
 router.use('/notification', notification);
 router.use('/docManager', documentManager);
+router.use('/modifyAccount', modifyAccount);
 
 router.use('/workbook', workbook);
-router.use('/creatework', creatework);
-router.use('/workcalendar', workcalendar);
-router.use('/worktemplate', worktemplate);
-router.use('/modifywork', modifywork);
+router.use('/createWork', createWork);
+router.use('/workCalendar', workCalendar);
+router.use('/workTemplate', workTemplate);
+router.use('/modifyWork', modifyWork);
 
-router.use('/billingpayment', billingpayment);
-router.use('/createinvoice', createinvoice);
+router.use('/billingPayment', billingPayment);
+router.use('/createInvoice', createInvoice);
 router.use('/invoices', invoices);
 router.use('/payment', payment);
-router.use('/modifyinvoice', modifyinvoice);
+router.use('/modifyInvoice', modifyInvoice);
 
 router.use('/newsletter', newsletter);
-router.use('/createNewsletter', createnewsletter);
+router.use('/createNewsletter', createNewsletter);
 
 router.use('/createStaff', createStaff);
 
