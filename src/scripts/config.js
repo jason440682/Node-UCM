0686fdf3d80c7d0dcf06624abc20c2124145adef
GYMing <gyming@outlook.com>
2017-03-28 20:35:37 +0800

    前端开发引入 RequireJS，做一个例子看看效果

---
+++ src/scripts/config.js
@@ -0,0 +1,9 @@
+requirejs.config({
+    baseUrl: '/scripts/lib',
+
+    paths: {
+        jquery: 'jquery-3.1.1.min',
+        bootstrap: 'bootstrap.min',
+        date_picker: 'My97DatePicker/WdatePicker.js'
+    }
+});
