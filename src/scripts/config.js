9c4804c4d4aa5cd64150ebf9e573204894396341
GYMing <gyming@outlook.com>
2017-06-24 23:38:52 +0800

    添加RequireJS 的声明

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -5,5 +5,11 @@ requirejs.config({
         jquery: 'jquery-3.1.1.min',
         bootstrap: 'bootstrap.min',
         date_picker: 'My97DatePicker/WdatePicker.js'
+    },
+    shim : {
+        // 声明 bootstrap 加载前需要加载 jquery
+        bootstrap : {
+            deps : ['jquery']
+        },
     }
 });
