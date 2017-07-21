37d6cd1d1ebd448415be432e5e85060943d129a5
GYMing <gyming@outlook.com>
2017-07-21 23:49:50 +0800

    修改测试的代码

--- src/scripts/user/login.js
+++ src/scripts/user/login.js
@@ -1,5 +1,5 @@
-alert('hello!');
-import $ from '../lib/jquery-3.1.1.min'
+const $ = window.jQuery;
+$(() => console.log('heiheihei'));
 const password = $('#password');
 password.blur(() => {
     console.log('Yes!')
@@ -25,28 +25,9 @@ password.blur(() => {
 });
 
 $('#verification-code-image').click(function () {
-    const data = { 'code': $('#verification-code')[0].value };
-    console.log('Send Ajax! Please wait...');
+    console.log('click!!');
+    let data = new FormData();
+    let array = $('form').serializeArray();
+    array.forEach(item => data.append(item.name, item.value));
     console.log(data);
-    $.ajax({
-        url: '/UCM/checkVerificationCode',
-        data: data,
-        type: 'post',
-        async: false,
-        success: function (data, textStatus) {
-            console.log('Success!');
-            console.log(data);
-            console.log(textStatus);
-        },
-        error: function (XMLHttpRequest, textStatus, errorThrown) {
-            console.log('Failed!');
-            console.log(XMLHttpRequest);
-            console.log(errorThrown);
-            console.log(XMLHttpRequest.responseText);
-        }
-    });
-    console.log('post end!');
-});
-require(['jquery'], function ($) {
-    
 });
