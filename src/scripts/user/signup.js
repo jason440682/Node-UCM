1b4131551082d4130dcaeb60ad07c0e2b0f46924
GYMing <gyming@outlook.com>
2017-07-21 23:44:53 +0800

    添加注册页面的逻辑代码

--- src/scripts/user/signup.js
+++ src/scripts/user/signup.js
@@ -1,1 +1,31 @@
-console.lo
+const $ = window.jQuery;
+
+$('#submit').click((event) => {
+    const data = {}
+    $('form').serializeArray().forEach(function(element) {
+        data[element.name] = element.value
+    }, this);
+    console.log(data)
+
+    $.ajax({
+        url: '/UCM/SignUp',
+        type: 'POST',
+        data: data,
+        contentType: "application/x-www-form-urlencoded",
+        beforeSend() {
+            console.log(this)
+            console.log('Send Ajax! Please wait...');
+        },
+        success(data, status) {
+            console.log('post success!')
+            console.log(data)
+            console.log(status)
+        },
+        error(xmlHttpRequest, status, errorThrown) {
+            console.log('Failed!');
+            console.log(xmlHttpRequest.status, xmlHttpRequest.responseText, errorThrown)
+        }
+    })
+
+    return false;
+})
