741eab379975801d1a602c6f9274e3392092314b
GYMing <gyming@outlook.com>
2017-08-01 00:32:18 +0800

    将项目的 jshint 换成 eslint 方便 es6语法检查

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "airbnb",
+        "plugin:jsx-a11y/recommended"
+    ],
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "plugins": [
+        "jsx-a11y"
+    ],
+    "rules": {
+        "comma-dangle": 1,
+        "global-require": 0,
+        "import/no-dynamic-require": 0,
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "no-param-reassign": [2, {
+            "props": false
+        }],
+        "no-underscore-dangle": 1,
+        "no-undef": 1,
+        "prefer-const": 1,
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "jsx-a11y/href-no-hash": 1,
+    }
+};
