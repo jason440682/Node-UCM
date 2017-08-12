22ae77d1ede7dfdf8b3a66aacb62fa03654308cb
GYMing <gyming@outlook.com>
2017-08-12 18:44:49 +0800

    添加新的接口

--- src/scripts/plugins/api.js
+++ src/scripts/plugins/api.js
@@ -1,34 +1,37 @@
 const $ = window.jQuery
 
 module.exports = {
+    createStaffUser(data) {
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                url: 'http://54.169.159.192:8080/UCM/CreateStaffUser',
+                type: 'POST',
+                data,
+            }).then(
+                (response, status, xhr) => resolve({ response, status, xhr }),
+                (xhr, status, error) => reject({ xhr, status, error }))
+        })
+    },
     getValidateCode() {
         return new Promise((resolve, reject) => {
             $.ajax({
                 url: '/api/validate',
                 type: 'GET',
             }).then(
-                (response, status, xhr) => {
-                    resolve({ response, status, xhr })
-                },
-                (xhr, status, error) => {
-                    reject({ xhr, status, error })
-                })
+                (response, status, xhr) => resolve({ response, status, xhr }),
+                (xhr, status, error) => reject({ xhr, status, error }))
         })
     },
     login(username, password) {
         const data = { userName: username, hashedPassword: password }
         return new Promise((resolve, reject) => {
             $.ajax({
-                url: 'http://54.169.159.192:8080/UCM/LogIn',
+                url: '/api/login',
                 type: 'POST',
                 data,
             }).then(
-                (response, status, xhr) => {
-                    resolve({ response, status, xhr })
-                },
-                (xhr, status, error) => {
-                    reject({ xhr, status, error })
-                })
+                (response, status, xhr) => resolve({ response, status, xhr }),
+                (xhr, status, error) => reject({ xhr, status, error }))
         })
     },
     sendValidateCode(code) {
@@ -38,12 +41,8 @@ module.exports = {
                 type: 'POST',
                 data: { code },
             }).then(
-                (response, status, xhr) => {
-                    resolve({ response, status, xhr })
-                },
-                (xhr, status, error) => {
-                    reject({ xhr, status, error })
-                })
+                (response, status, xhr) => resolve({ response, status, xhr }),
+                (xhr, status, error) => reject({ xhr, status, error }))
         })
     },
     signUp(data) {
@@ -53,12 +52,8 @@ module.exports = {
                 type: 'POST',
                 data,
             }).then(
-                (response, status, xhr) => {
-                    resolve({ response, status, xhr })
-                },
-                (xhr, status, error) => {
-                    reject({ xhr, status, error })
-                })
+                (response, status, xhr) => resolve({ response, status, xhr }),
+                (xhr, status, error) => reject({ xhr, status, error }))
         })
     },
     uploadLogo(data) {
@@ -70,12 +65,8 @@ module.exports = {
                 processData: false,
                 contentType: false,
             }).then(
-                (response, status, xhr) => {
-                    resolve({ response, status, xhr })
-                },
-                (xhr, status, error) => {
-                    reject({ xhr, status, error })
-                })
+                (response, status, xhr) => resolve({ response, status, xhr }),
+                (xhr, status, error) => reject({ xhr, status, error }))
         })
     },
 }
