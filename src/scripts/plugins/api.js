13d2168166bad14bc9d5c1730c45c0c86324cdaa
GYMing <gyming@outlook.com>
2017-08-26 23:42:27 +0800

    添加新的接口，并对代码做了一些整理

--- src/scripts/plugins/api.js
+++ src/scripts/plugins/api.js
@@ -1,83 +1,79 @@
 const $ = window.jQuery
 
+function ajax(options) {
+    return new Promise((resolve, reject) => {
+        $.ajax(options).then(
+            (response, status, xhr) => resolve({ response, status, xhr }),
+            (xhr, status, error) => reject({ xhr, status, error }))
+    })
+}
+
 module.exports = {
     createClientAccounts(data) {
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                url: 'http://54.169.159.192:8080/UCM/CreateCustomerAccount',
-                type: 'POST',
-                data,
-            }).then(
-                (response, status, xhr) => resolve({ response, status, xhr }),
-                (xhr, status, error) => reject({ xhr, status, error }))
+        return ajax({
+            url: 'http://54.169.159.192:8080/UCM/CreateCustomerAccount',
+            type: 'POST',
+            data,
         })
     },
     createStaffUser(data) {
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                url: 'http://54.169.159.192:8080/UCM/CreateStaffUser',
-                type: 'POST',
-                data,
-            }).then(
-                (response, status, xhr) => resolve({ response, status, xhr }),
-                (xhr, status, error) => reject({ xhr, status, error }))
+        return ajax({
+            url: 'http://54.169.159.192:8080/UCM/CreateStaffUser',
+            type: 'POST',
+            data,
         })
     },
     getValidateCode() {
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                url: '/api/validate',
-                type: 'GET',
-            }).then(
-                (response, status, xhr) => resolve({ response, status, xhr }),
-                (xhr, status, error) => reject({ xhr, status, error }))
+        return ajax({
+            url: '/api/validate',
+            type: 'GET',
         })
     },
     login(username, password) {
         const data = { userName: username, hashedPassword: password }
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                url: '/api/user/login',
-                type: 'POST',
-                data,
-            }).then(
-                (response, status, xhr) => resolve({ response, status, xhr }),
-                (xhr, status, error) => reject({ xhr, status, error }))
+        return ajax({
+            url: '/api/user/login',
+            type: 'POST',
+            data,
         })
     },
     sendValidateCode(code) {
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                url: '/api/validate',
-                type: 'POST',
-                data: { code },
-            }).then(
-                (response, status, xhr) => resolve({ response, status, xhr }),
-                (xhr, status, error) => reject({ xhr, status, error }))
+        return ajax({
+            url: '/api/validate',
+            type: 'POST',
+            data: { code },
         })
     },
     signUp(data) {
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                url: 'http://54.169.159.192:8080/UCM/SignUp',
-                type: 'POST',
-                data,
-            }).then(
-                (response, status, xhr) => resolve({ response, status, xhr }),
-                (xhr, status, error) => reject({ xhr, status, error }))
+        return ajax({
+            url: 'http://54.169.159.192:8080/UCM/SignUp',
+            type: 'POST',
+            data,
         })
     },
     uploadLogo(data) {
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                url: 'http://54.169.159.192:8080/UCM/uploadLogo',
-                type: 'POST',
-                data,
-                processData: false,
-                contentType: false,
-            }).then(
-                (response, status, xhr) => resolve({ response, status, xhr }),
-                (xhr, status, error) => reject({ xhr, status, error }))
+        return ajax({
+            url: 'http://54.169.159.192:8080/UCM/uploadLogo',
+            type: 'POST',
+            data,
+            processData: false,
+            contentType: false,
+        })
+    },
+    saveClientChange(data) {
+        return ajax({
+            url: 'http://54.169.159.192:8080/UCM/ModifyCustomerAccount',
+            type: 'POST',
+            data,
+        })
+    },
+    deleteClient(userName, customerId) {
+        const data = { userName, customerId }
+        console.log(data)
+        return ajax({
+            url: 'http://54.169.159.192:8080/UCM/DeleteClient',
+            type: 'POST',
+            data,
         })
     },
 }
