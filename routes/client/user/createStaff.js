fb79fb7ac865255a4f7cf0b7741ce95b8c1c8394
GYMing <gyming@outlook.com>
2017-08-27 13:59:53 +0800

    给 createStaff 添加导航栏

--- routes/client/user/createStaff.js
+++ routes/client/user/createStaff.js
@@ -6,14 +6,16 @@ const router = Router()
 router.get('/', (req, res) => {
     console.log('Get Create Staff')
     const lang = req.lang ? req.lang : 'en'
+    const userName = req.session.userName
     const data = {
         key: 'user/createStaff',
         language: lang,
         lang: require(`./lang/${lang}/createStaff`),
+        nav: require(`../public/lang/${lang}/navbar`),
+        user: userName,
     }
 
     Promise.all([getTimezones(), getCountries()]).then(([timezones, countries]) => {
-        console.log('success')
         data.timezones = timezones.body
         data.countries = countries.body
 
