749f18a2fcdf7b087deebc326092f1856d5e9c1f
GYMing <gyming@outlook.com>
2017-09-01 11:35:27 +0800

    添加 http-proxy-middleware 的测试配置

--- config/index.js
+++ config/index.js
@@ -3,6 +3,17 @@ module.exports = {
         '/UCM': {
             target: 'http://54.169.159.192:8080/',
             changeOrigin: true
+        },
+        '/test': {
+            target: 'https://httpbin.org/post?show_env=1',
+            changeOrigin: true
+        },
+        '/hehe': {
+            target: 'http://localhost:3333/api',
+            // changeOrigin: true,
+            pathRewrite: {
+                '/hehe': '/test'
+            }
         }
     }
 }
