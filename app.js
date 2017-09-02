1e1d5d74bab727fc065727983d9d57b3afb5d0c0
GYMing <gyming@outlook.com>
2017-09-02 15:15:26 +0800

    添加 http-proxy-middleware

--- app.js
+++ app.js
@@ -16,6 +16,40 @@ app.set('views', path.join(__dirname, './views'));
 // 渲染页面的语言
 app.set('view engine', 'pug');
 
+console.log("process.env.NODE_ENV: " + app.get('env'));
+// 判断系统环境，如果是开发环境，则启用 http-proxy
+if (app.get('env') === 'development') {
+    var proxyMiddleware = require('http-proxy-middleware');
+    var proxyTable = config.proxyTable;
+    Object.keys(proxyTable).forEach(function (context) {
+        var options = proxyTable[context];
+        if (typeof options === 'string') {
+            options = { target: options };
+        }
+        app.use(proxyMiddleware(options.filter || context, options))
+    })
+
+    // development error handler
+    // will print stacktrace
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+} else {
+    // production error handler
+    // no stacktraces leaked to user
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
+    });
+}
+
 // Express 日志组件
 app.use(morgan('dev'));
 // 处理 JSON 数据
@@ -42,20 +76,6 @@ app.use(function (req, res, next) {
     next();
 });
 
-console.log("process.env.NODE_ENV: " + app.get('env'));
-// 判断系统环境，如果是开发环境，则启用 http-proxy
-if (app.get('env') === 'development') {
-    var proxyMiddleware = require('http-proxy-middleware');
-    var proxyTable = config.proxyTable;
-    Object.keys(proxyTable).forEach(function (context) {
-        var options = proxyTable[context];
-        if (typeof options === 'string') {
-            options = { target: options };
-        }
-        app.use(proxyMiddleware(options.filter || context, options))
-    })
-}
-
 app.use('/', routes);
 
 // catch 404 and forward to error handler
@@ -63,30 +83,6 @@ app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
 });
 
 module.exports = app;
